2ef16acfabbca433eca2f45de7a3cf7172952f59
Ni3-Jadhav <ntjadhav2000@gmail.com>
2026-01-14 15:18:53 +0530

    Initial commit

---
+++ Models/paymentModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const paymentSchema = new mongoose.Schema({
+    userName: String,
+
+    mobile: String,
+
+    product: String,
+
+    amount: Number,
+
+    transactionID: String,
+
+    merchantUserId : String,
+
+    paymentStatus: String,
+
+    paymentResponse: Object,
+
+
+});
+
+const PaymentDB = mongoose.model('paymentDB', paymentSchema);
+
+module.exports = PaymentDB;
